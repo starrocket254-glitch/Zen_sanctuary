e8f453824a18e0fe936adb496e668396a317934c
starrocket254-glitch <starrocket254@gmail.com>
2026-05-14 15:00:58 +0300

    Set up routing for main application pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Treatments from "./pages/Treatments";
+import Book from "./pages/Book";
+import Contact from "./pages/Contact";
 
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/treatments" element={<Treatments />} />
+      <Route path="/book" element={<Book />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+}
